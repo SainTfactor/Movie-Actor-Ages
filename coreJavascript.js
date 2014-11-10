789b21ab36db3551a0733cf9f4fe971ade547366
Sean Tyler Atwell <sta0003@auburn.edu>
2014-11-09 23:53:45 -0600

    Added "movies in theater" polling functionality
    I had to use a library to give me cross domain request functionality.
    The library uses a cool service yahoo offers to parse requests for you
    and feed you the results.  It's pretty cool stuff.

--- coreJavascript.js
+++ coreJavascript.js
@@ -1,9 +1,42 @@
 $(function() {
-
-$.getJSON("http://www.imdb.com/movies-in-theaters/", function(response) {
-	$.each(response.result, function(i,planet){
-		$('<div>').appendTo(document.body);
-	});
-});
+    
+   var arrayOfMovies = new Array();
+    
+    $.ajax({
+        url: "http://www.google.com/movies",
+        type: "GET",
+        data: { "near" : "36375" },
+    })
+    .done(function(data) {
+        
+        var html = $.parseHTML(data.results[0]); 
+        
+        for(var i = 0; i < $(html).find("div.theater").length; i++)
+        {
+            var theater = $(html).find("div.theater")[i];
+            for(var j = 0; j < $(theater).find(".showtimes .show_left .movie").length; j++)
+            {
+                var movie = $(theater).find(".showtimes .show_left .movie")[j];
+                if($.inArray($(movie).find(".name a")[0].innerHTML, arrayOfMovies))
+                    arrayOfMovies[arrayOfMovies.length] = $(movie).find(".name a")[0].innerHTML;
+            }
+            for(var j = 0; j < $(theater).find(".showtimes .show_right .movie").length; j++)
+            {
+                var movie = $(theater).find(".showtimes .show_right .movie")[j];
+                if($.inArray($(movie).find(".name a")[0].innerHTML, arrayOfMovies))
+                    arrayOfMovies[arrayOfMovies.length] = $(movie).find(".name a")[0].innerHTML;
+            }
+        }
+        
+        console.log(arrayOfMovies);
+    });
+    
+    /*$.getJSON("http://moviefone.com/showtimes?locationQuery=36830", function(response) {
+        $.each(response.result, function(i,planet){
+            $('<div>').appendTo(document.body);
+        });
+    });*/
+    
+    
 
 });
