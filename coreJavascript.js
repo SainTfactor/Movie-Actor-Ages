cb5049f0b0d1b95ea0ac8912d18ae92cb3f3e8d5
Sean Tyler Atwell <sta0003@auburn.edu>
2014-11-10 12:58:18 -0600

    I am now pulling the data from freebase
    I will probably switch away from freebase in the next iteration for a
    few reasons.
    1 - it hard limits the number of queries you can run
    2 - it's data on actors and movies is woefully incomplete (nightcrawler
    and oujia arent even there)

--- coreJavascript.js
+++ coreJavascript.js
@@ -1,7 +1,6 @@
 $(function() {
 
     var arrayOfMovies = new Array(); // array of currently playing movies
-    var arrayOfActors = new Array(); // array of currently playing movies
     
     //fetch the list of relevant movies from google.
     $.ajax({
@@ -25,15 +24,19 @@ $(function() {
                 // get all the movies
                 var movie = $(theater).find(".showtimes .show_left .movie")[j];
                 //test and see if we already have them in the list
-                if($.inArray($(movie).find(".name a")[0].innerHTML, arrayOfMovies))
+                if ($.inArray($(movie).find(".name a")[0].innerHTML, arrayOfMovies) == -1) {
+                    $('<div>', { text: $(movie).find(".name a")[0].innerHTML, id: $(movie).find(".name a")[0].innerHTML }).appendTo(document.body);
                     arrayOfMovies[arrayOfMovies.length] = $(movie).find(".name a")[0].innerHTML; // add them to the list
+                }
             }
             // same as above but for the right column
             for(var j = 0; j < $(theater).find(".showtimes .show_right .movie").length; j++)
             {
                 var movie = $(theater).find(".showtimes .show_right .movie")[j];
-                if($.inArray($(movie).find(".name a")[0].innerHTML, arrayOfMovies))
+                if ($.inArray($(movie).find(".name a")[0].innerHTML, arrayOfMovies) == -1) {
+                    $('<div>', { text: $(movie).find(".name a")[0].innerHTML, id: $(movie).find(".name a")[0].innerHTML }).appendTo(document.body);
                     arrayOfMovies[arrayOfMovies.length] = $(movie).find(".name a")[0].innerHTML;
+                }
             }
         } 
         
@@ -48,12 +51,21 @@ $(function() {
             var query = [{"type": "/film/film","name": value,"/film/film/directed_by": null,"/film/film/initial_release_date>": dateStr,"/film/film/starring": [{"actor": null,"mid": null}]}];
             var service_url = 'https://www.googleapis.com/freebase/v1/mqlread';
             $.getJSON(service_url + '?callback=?', {query:JSON.stringify(query)}, function(response) {
-                if(response.result.length != 0)
-                {                    
+                if (response.result.length != 0) {
                     // just stringifying for now.  Ill make it pretty later
+
+                    /*
                     $('<div>',{text:JSON.stringify(response.result[0]["/film/film/starring"])}).appendTo(document.body);
                     $('<br>').appendTo(document.body);
                     $('<br>').appendTo(document.body);
+                    */
+
+                    $("#" + value).html($("#" + value).html() + " - " + JSON.stringify(response.result[0]["/film/film/starring"]));
+
+                    //arrayOfActors[arrayOfActors.length] = [response.result[0]["name"], JSON.stringify(response.result[0]["/film/film/starring"])];
+                }
+                else {
+                    $("#" + value).css("display", "none");
                 }
             });
         });
